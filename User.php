d94a0213a259bde7f62a72d036674e5b3bef772b
Lenovo <asadbek.polatov.02@mail.ru>
2022-09-16 14:15:10 +0500

    main page created

---
+++ User.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once "connect.php";
+
+class User
+{
+    public $chat_id;
+
+    public function __construct($chat_id)
+    {
+        $this->chat_id = $chat_id;
+    }
+
+    public function createUser(){
+        global $connect;
+        $sql = "DELETE users FROM WHERE chat_id = $this->chat_id";
+        $connect->query($sql);
+        $sql = "INSERT INTO users(chat_id) VALUES($this->chat_id)";
+        $connect->query($sql);
+    }
+
+    public function setPage($page){
+        global $connect;
+        $sql = "UPDATE users SET page='{$page}' WHERE chat_id = $this->chat_id";
+        $connect->query($sql);
+    }
+
+    public function getPage(){
+        global $connect;
+        $sql = "SELECT page FROM users WHERE chat_id = $this->chat_id";
+        $result = $connect->query($sql);
+        $row = $result->fetch_assoc();
+        return $row['page'];
+    }
+
+    public function setLanguage($language){
+        global $connect;
+        $sql = "UPDATE users SET language='$language' WHERE chat_id = $this->chat_id";
+        $connect->query($sql);
+    }
+
+    public function getLanguage(){
+        global $connect;
+        $sql = "SELECT `language` FROM users WHERE chat_id = $this->chat_id";
+        $result = $connect->query($sql);
+        $row = $result->fetch_assoc();
+        return $row['language'];
+    }
+
+    public function GetText($keyword){
+        global $connect;
+
+        $language = $this->getLanguage();
+        $sql = "SELECT * FROM texts WHERE keyword = '{$keyword}'";
+        $result = $connect->query($sql);
+        $row = $result->fetch_assoc();
+        return $row[$language];
+    }
+}
--- index.php
+++ index.php
@@ -1,22 +1,40 @@
 <?php
 
-require_once 'connect.php';
 include 'Telegram.php';
 
 $bot_token = "5645695314:AAGiw1PMNkMLbzqc-RVWYOMtG-QQPpdCnjY";
 $telegram = new Telegram($bot_token);
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
-$firstname = $telegram->FirstName();
+$data = $telegram->getData();
+$message = $data['message'];
 
-//$user = new User($chat_id);
-
+$user = new User($chat_id);
 //$admin_chat_id = 967469906;
 
-$content = [
-    'chat_id' => $chat_id,
-    'text' => $text,
-];
-$telegram->sendMessage($content);
+if ($text == "/start"){
+    showMainPage();
+}
+
+function showMainPage(){
+    global $chat_id,$telegram, $user;
+    $user->createUser();
+    $user->setPage("main");
+    $user->setLanguage("uz");
+
+    $text = $user->GetText("main");
+
+    $options = [
+        [$telegram->buildKeyboardButton("button")]
+    ];
+    $keyboard = $telegram->buildKeyBoard($options,false, true);
+    $content = [
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyboard,
+        'text' => $text,
+    ];
+    $telegram->sendMessage($content);
+}
+
 
 ?>
